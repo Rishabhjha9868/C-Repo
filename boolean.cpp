ac8dcaed28b960664b22592582652e2f12dbcf28
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-10-15 23:40:55 +0530

    Add files via upload

---
+++ boolean.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+bool isCodingFun = true;
+bool isFishTasty = false;
+cout << isCodingFun;  // Outputs 1 (true)
+cout << isFishTasty; 
+return 0;
+}
+
+ // Outputs 0 (false)
+
+
