1c091ec4259e36e56272f674dbfb00c54c302941
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-11-01 20:52:13 +0530

    Add files via upload

---
+++ sumw3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void sum(){
+cout<< "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+    cout << " The sum of 29 and 30 is : "<< 29+30 <<"\n\n" ;
+}
+int main()
+{
+  sum();
+    
+}
