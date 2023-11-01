d124a066f1c7aea5d00090387413667ae889b3b1
Rishabhjha9868 <147179883+Rishabhjha9868@users.noreply.github.com>
2023-11-01 20:51:42 +0530

    Add files via upload

---
+++ sumvariable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ void var(){
+   cout << "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+	int a;
+	int b;
+	int sum;
+	a=29;
+	b=30;
+	sum=a+b;
+	cout << " The sum of "<< a << " and "<<b <<" is : "<< sum <<"\n\n" ;
+
+ }
+int main()
+{
+  var();
+   
+}
